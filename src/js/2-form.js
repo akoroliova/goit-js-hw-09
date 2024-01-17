8ba1ed46db9acac1e92fcffae7ff6f01fd5e00e6
Oleksandra K <a.koroliova@gmail.com>
2024-01-17 14:09:38 +0200

    Add task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,51 +1,44 @@
 const feedbackForm = document.querySelector('form.feedback-form');
+const localStorageKey = 'feedback-form-state';
 
-feedbackForm.addEventListener('input', handleInput);
+const populateFieldsFromLocalStorage = () => {
+  const storedObject = localStorage.getItem(localStorageKey);
+  if (storedObject === null) {
+    return;
+  }
+  const parsedStoredObject = JSON.parse(storedObject);
+  feedbackForm.elements.email.value = parsedStoredObject.email;
+  feedbackForm.elements.message.value = parsedStoredObject.message;
+};
+populateFieldsFromLocalStorage();
+
+const getInputValues = () => {
+  const email = feedbackForm.elements.email.value.trim();
+  const message = feedbackForm.elements.message.value.trim();
+  return { email, message };
+};
 
 function handleInput(event) {
-  const target = event.target;
+  const values = getInputValues();
 
-  if (target.tagName === 'input' || target.tagName === 'textarea') {
-    const email = feedbackForm.elements.email.value.trim();
-    const message = feedbackForm.elements.message.value.trim();
-
-    const inputObject = {};
-    inputObject.email = email;
-    inputObject.message = message;
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(inputObject));
-
-    console.log(`This is "localStorage" DURING input: `);
-    console.log(localStorage);
-  }
+  localStorage.setItem(localStorageKey, JSON.stringify(values));
 }
-
-console.log(`This is "localStorage" AFTER input: `);
-console.log(localStorage);
-
-feedbackForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  const values = getInputValues();
 
-  const email = feedbackForm.elements.email.value;
-  const message = feedbackForm.elements.message.value;
-  if (email === '' || message === '') {
+  if (values.email === '' || values.message === '') {
     return alert('All form fields must be filled in');
   }
 
-  const target = event.target;
-  if (target.tagName === 'button') {
-    feedbackForm.reset();
-    console.log(`This is "localStorage" after form reset: `);
-    console.log(localStorage);
+  const parsedStoredObject = JSON.parse(localStorage.getItem(localStorageKey));
+  console.log(parsedStoredObject);
 
-    localStorage.clear();
-    console.log(`This is "localStorage" after localStorage clear: `);
-    console.log(localStorage);
-  }
+  localStorage.removeItem(localStorageKey);
+
+  feedbackForm.reset();
 }
 
-// Введення даних в одне поле форми не видаляє дані в сховищі для іншого
-
-// 2 Під час (пере)завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, то заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми. Тобто При оновленні сторінки дані з локального сховища підставляються в елементи форми, у полях форми відсутні undefined. Потрібно перевіряти всі значення, які ви читаєте з localStorage і записуєте в поля форми. Якщо там пусто, вам у поля запишеться undefined.
+feedbackForm.addEventListener('input', handleInput);
+feedbackForm.addEventListener('submit', handleSubmit);
